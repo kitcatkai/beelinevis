d0b3dc0e13229dd68ca656c2dfcedc5cce2727a6
Tan Kai Wei <tankaiwei@Tans-MacBook-Pro.local>
2018-04-15 20:27:34 +0800

    change server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
-app.use('/src/assets', express.static(__dirname + '/src/assets/'));
+// app.use(express.static(__dirname + '/dist/'));
+// app.use('/src/assets', express.static(__dirname + '/src/assets/'));
 
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
